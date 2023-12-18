4e52c958b6569aa24c2e100073104c9a3aeccdbc
Realnotema <tematsyganckov@yandex.ru>
2023-12-19 00:13:17 +0300

    added Makefile and refact

--- netscan/netscan.c
+++ netscan/netscan.c
@@ -4,14 +4,10 @@
 #include <arpa/inet.h>      // < for sizeof(sockaddr_in)
 #include <unistd.h>
 #include <string.h>
+#include <netdb.h>
 
 #define BUFLEN 512          // < max len of bufer
 #define TIMEOUT_SECONDS 5
-
-void printerr(char *errmess) {
-    perror(errmess);
-    exit(1);
-}
 
 void scanUDP(char *ip, char *port) {
     int sockfd;
@@ -82,38 +78,4 @@ void scanTCPconnect(char *ip, char *port) {
 
     printf("Port %d is available.\n", atoi(port));
     close(sockfd);
-}
-
-void read_flag(char *argv[]) {
-    if (strcmp(argv[1], "-U") == 0) {
-        printf("UDP scanning is selected\n");
-        scanUDP(argv[2], argv[3]);
-    } else if (strcmp(argv[1], "-T") == 0) {
-        printf("TCP connect scanning is selected\n");
-        scanTCPconnect(argv[2], argv[3]);
-    } else if (strcmp(argv[1], "-sT") == 0) {
-        printf("TCP SYN scanning is selected\n");
-    } else printerr("Incorrect flag: --help for more information");
-}
-
-int main(int argc, char* argv[]) {
-    switch(argc) {
-        case 1:
-            printerr("Incorrect entry: --help for more information");
-            break;
-        case 2:
-            if (strcmp(argv[1], "--help") == 0) {
-                printf("\nUse ./netscan -<flag> <ip> <port>\n\n"
-                "Flags:\n\t-U - to UDP scan\n\t-T - to TCP connect scan\n\t-sT - to TCP SYN scan\n"
-                "In this version you cannot combine flags\n\n");
-            }
-            break;
-        case 3:
-            printerr("Incorrect entry: --help for more information");
-            break;
-        case 4:
-            read_flag(argv);
-    }
-    printf("QUITING!\n");
-    return 0;
 }
---
+++ netscan/netscan.h
@@ -0,0 +1,2 @@
+void scanUDP(char *ip, char *port);
+void scanTCPconnect(char *ip, char *port);
